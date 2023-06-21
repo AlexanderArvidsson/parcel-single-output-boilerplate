a2f5a0a9473884354807a1d81ac6cf6975e65825
Alexander Arvidsson <alexander@arvidson.nu>
2023-06-21 13:35:27 +0200

    Add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
 const App: React.FC = () => {
-  return <div>This is a React app!</div>;
+  return (
+    <div className="page">
+      <Card>This is a React app!</Card>
+    </div>
+  );
 };
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const element = document.getElementById('app');
-
 const root = createRoot(element!);
 
 root.render(<App />);
