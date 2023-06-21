9a3d546bf6ec66063987cddf42480b59b73b1a10
Alexander Arvidsson <alexander@arvidson.nu>
2023-06-21 13:27:48 +0200

    Initial code

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+const App: React.FC = () => {
+  return <div>This is a React app!</div>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+const element = document.getElementById('app');
+
+const root = createRoot(element!);
+
+root.render(<App />);
